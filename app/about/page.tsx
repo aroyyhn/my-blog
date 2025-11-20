8c1b42a29d7d85d2912e07bf0594bd2f81bcfd39
Royhan <imroyhan69@gmail.com>
2025-11-20 11:31:38 +0700

    tentang

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,13 +6,8 @@ export default function About() {
         TENTANG PENULIS
       </h1>
 
-      <p className="text-[15px] text-gray-600 leading-relaxed">
-        Lahir di pedalaman Cirebon. Tumbuh besar di antara suara katak dan tumpukan baju yang belum di lipat.
-        Aku bukan siapa-siapa, hanya manusia yang mendedikasikan hidupnya untuk selalu belajar.
-        <br /><br />
-        Mulai suka nulis juga gak inget sejak kapan.
-        <br />
-        Intinya kalo suatu saat di tanya kenapa suka nulis? jawabannya: pengen bikin orang senyum.
+      <p className="text-[14px] text-gray-600 leading-relaxed text-decoration-line: underline">
+      MANUSIA YANG INGIN MENGHADIRKAN SENYUMAN BAGI BANYAK ORANG LEWAT TULISANNYA.
       </p>
 
     </div>
